accd949df474ba1c190b653a6457efc6cff1ffbf
HyperExtendedReality <139488800+HyperExtendedReality@users.noreply.github.com>
2024-06-25 11:28:22 -0400

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -19,7 +19,7 @@ var speakWord = "Good Bye";
   byeSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   }
-})(window);
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
-window.byeSpeaker = byeSpeaker;
+  window.byeSpeaker = byeSpeaker;
+})(window);
