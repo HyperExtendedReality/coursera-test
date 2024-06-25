1b9cd9d0e808c941ec67f0570b08d63778700e1f
HyperExtendedReality <139488800+HyperExtendedReality@users.noreply.github.com>
2024-06-25 11:27:56 -0400

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -16,9 +16,10 @@ var speakWord = "Hello";
 helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
-})(window);
+
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
- window.helloSpeaker = helloSpeaker;
+  window.helloSpeaker = helloSpeaker;
+})(window);
