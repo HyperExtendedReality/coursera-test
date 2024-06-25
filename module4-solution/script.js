094613aa4fe5b4b255a4b905d00cfea9c0710325
HyperExtendedReality <139488800+HyperExtendedReality@users.noreply.github.com>
2024-06-25 11:46:03 -0400

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -51,7 +51,6 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
      var firstLetter = names[name].charAt(0).toLowerCase;
-    console.log(firstLetter);
   
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
@@ -59,9 +58,9 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
     if (firstLetter == 'j') {
-       byeSpeaker.speak(name);
+       byeSpeaker.speak(names[name]);
     } else {
-       helloSpeaker.speak(name);
+       helloSpeaker.speak(names[name]);
     }
   }
 })();
