9e63bb6783173a45f1dd904dd60ac7bc6fa50af8
HyperExtendedReality <139488800+HyperExtendedReality@users.noreply.github.com>
2024-06-25 11:37:24 -0400

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -58,9 +58,9 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
     if (firstLetter == 'j') {
-       byeSpeaker(name);
+       byeSpeaker.speak(name);
     } else {
-       helloSpeaker(name);
+       helloSpeaker.speak(name);
     }
   }
 })();
